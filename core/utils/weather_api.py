07998b43090b810f6195aff4311008d7d2eb65c9
Mateusz <mateuszporadzisz225@gmail.com>
2025-04-11 12:27:54 +0200

    added historical_view.html and historical data handling in views.py, focusing now on displaying it on frontend

--- core/utils/weather_api.py
+++ core/utils/weather_api.py
@@ -8,13 +8,14 @@ class WeatherAPI:
         self.base_url = "https://api.openweathermap.org/data/2.5/"
         self.history_url = "https://history.openweathermap.org/data/2.5/history/"
         self.api_key = Config.API_KEY
-
     def _make_request(self, endpoint, params=None):
         if params is None:
             params = {}
         params["appid"] = self.api_key
         response = requests.get(endpoint, params=params)
         response.raise_for_status()
+        print("TUTAJ")
+        print(response.url)
         return response.json()
 
     def get_weather(self, request_type, city, **kwargs):
@@ -29,7 +30,7 @@ class WeatherAPI:
         elif request_type == "historical":
             date = kwargs.get('date', datetime.now())
             days = min(kwargs.get('days', 1), 7) #Jestem dumny z tego zapisu
-            start = int(date.timestamp())
+            start = int(date.timestamp()-864000)
             end = start + days * 86400
             params.update({"start": start, "end": end})
             endpoint = f"{self.history_url}city"
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from datetime import datetime
 
 # Create your views here.
 from .utils.weather_api import WeatherAPI
@@ -12,17 +12,48 @@ def weather_view(request):
 
     try:
         city = request.GET.get('city', 'Warsaw')
+        view_type = request.GET.get('view_type')
         api = WeatherAPI()
-        weather_data = api.get_weather('current', city)
-        context = {
-            'city': city,
-            'temperature': round(weather_data['main']['temp'], 1),
-            'description': weather_data['weather'][0]['description'].capitalize(),
-            'icon': weather_data['weather'][0]['icon'],
-            'humidity': weather_data['main']['humidity'],
-            'wind_speed': weather_data['wind']['speed'],
-            'success': True
-        }
+        weather_data = api.get_weather(view_type, city)
+        if view_type == 'current':
+            context = {
+                'city': city,
+                'temperature': round(weather_data['main']['temp'], 1),
+                'description': weather_data['weather'][0]['description'].capitalize(),
+                'icon': weather_data['weather'][0]['icon'],
+                'humidity': weather_data['main']['humidity'],
+                'wind_speed': weather_data['wind']['speed'],
+                'success': True
+            }
+            return render(request, 'core/weather_view.html', context)
+        elif view_type == 'historical':
+            historical_list = []
+            for i, hour_data in enumerate(weather_data['list']):
+                dt = hour_data['dt']
+                hour = datetime.fromtimestamp(dt).strftime('%H:%M')
+                print(hour)
+                if i > 0:
+                    prev_dt = weather_data['list'][i - 1]['dt']
+                    delta_hours = int((dt - prev_dt) / 3600)
+                    print(delta_hours)
+                    historical_list.append({
+                        'hour': hour,
+                        'temp': round(hour_data['main']['temp'], 1),
+                        'feels_like': round(hour_data['main']['feels_like'], 1),
+                        'humidity': hour_data['main']['humidity'],
+                        'wind_speed': hour_data['wind']['speed'],
+                        'description': hour_data['weather'][0]['description'].capitalize(),
+                        'delta_hours': delta_hours
+                    })
+                else:
+                    print(None)
+                    delta_hours = None
+
+            context = {
+                'city': city,
+                'forecast_list': historical_list,
+                'success': True
+            }
+            return render(request, 'core/historical_view.html', {'context': context})
     except Exception as e:
         context['error'] = f"Błąd: {str(e)}"
-    return render(request, 'core/weather_view.html', context)
